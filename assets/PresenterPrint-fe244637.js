46bfe46c93b8b81bee4a92279d61049cafb25633
urnotlynley <543636665@qq.com>
2023-04-23 12:13:10 +0800

    动画

--- assets/PresenterPrint-fe244637.js
+++ assets/PresenterPrint-fe244637.js
@@ -1,4 +1,4 @@
-import{d,i as _,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as l,k as b,l as N,m as k,p as P,q as w,_ as S}from"./index-f9b4bd7c.js";import{N as V}from"./NoteDisplay-1a637130.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},q=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},E=d({__name:"PresenterPrint",setup(F){_(p),h(`
+import{d,i as _,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as l,k as b,l as N,m as k,p as P,q as w,_ as S}from"./index-6b588584.js";import{N as V}from"./NoteDisplay-e914a80e.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},q=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},E=d({__name:"PresenterPrint",setup(F){_(p),h(`
 @page {
   size: A4;
   margin-top: 1.5cm;
